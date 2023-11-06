b768755ed458bf17a2cba3513c1d4014e594f6da
azzoz20011 <118281863+azzoz20011@users.noreply.github.com>
2023-11-06 19:56:14 +0300

    BE_1
    I have finished BE_1 in three files. The other changes in the files is not important for now

---
+++ HTML-1-exercise_2/class article{.js
@@ -0,0 +1,20 @@
+class article{
+    constructor(listOfArticles){
+    this.listOfArticles = listOfArticles
+    this.articleList = []
+    }
+    static createArticle (title, text) {
+        const li = document.createElement('li')
+        const h3 = document.createElement('h3')
+        const para = document.createElement('p')
+        h3.appendChild(title)
+        para.appendChild(text)
+        li.appendChild(h3)
+        li.appendChild(para)
+        return li
+    }
+    add{
+
+    }   
+    for (const)
+}
